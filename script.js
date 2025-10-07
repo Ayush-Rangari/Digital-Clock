545bfb6f81a7b8a2e386d01019f02ef9cfbbd2ca
Ayush-Rangari <rangariayush149@gmail.com>
2025-10-07 23:18:37 +0530

    Updated the features

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 function digitalClock() {
-    let period;
-    const now = new Date;
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const date = now.getDate();
-    const month = now.getMonth();
-    const year = now.getFullYear();
+  let period;
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const date = now.getDate();
+  const month = now.getMonth();
+  const year = now.getFullYear();
+  const day = now.getDay();
 
+  const time = document.querySelector(".time");
+  const info = document.querySelector(".dateInfo");
 
-    const time = document.querySelector('.time');
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
+  if (hours >= 0 && hours < 12) {
+    period = "AM";
+    
+  } else {
+    period = "PM";
+  }
 
-    if (hours >= 0 && hours < 12) {
-        period = 'AM'
-    }else {
+  time.textContent = `${hours} : ${minutes} : ${seconds} ${period}`;
 
-        period = 'PM'
-    }
-    time.innerHTML = `${hours} : ${minutes} : ${seconds} ${period}`;
-
-
+  info.textContent = `${date} ${months[month]} ${year}, ${days[day]}`;
 }
 setInterval(digitalClock, 1000);
 
-digitalClock()
+digitalClock();
